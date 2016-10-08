528d78d57c380e0a2c17758ddb8d1c648080f756
sbtjavaschool <sbt.java.school@gmail.com>
2016-10-08 12:58:57 +0300

    .

--- src/ru/sbt/visitor/Main.java
+++ src/ru/sbt/visitor/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Point point = new Point3D();
 
+        System.out.println("HELLO");
+
         double result = point.visit(new SqrtVisitor());
         point.visit(new PrintDetailsVisitor());
     }
